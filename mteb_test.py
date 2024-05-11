bf9ad782820a6d3ce0b72c9d9d275a7e4e31bfa4
Shahar Satamkar <shahar.satamkar@fiverr.com>
2024-05-11 16:36:52 +0300

    .

--- mteb_test.py
+++ mteb_test.py
@@ -6,9 +6,12 @@ from EasyLM.models.rpt.rpt_model_torch import RPTForCausalLM
 
 model = RPTForCausalLM.from_pretrained('shahar603/rpt-torch-1')
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model = nn.DataParallel(model)
+device = 'cuda:1' if torch.cuda.is_available() else 'cpu'
 model.to(device)
+
+#model.push_to_hub(repo_id="rpt-torch-1", token='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')
+
+#model.encode(['hello world', 'this is a test'])
 
 evaluation = MTEB(tasks=["Banking77Classification"])
 results = evaluation.run(model, output_folder=f"results/'rpt")
