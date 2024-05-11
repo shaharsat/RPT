20642275db8a8f88b4e19898699f8716431c3997
Shahar Satamkar <shahar.satamkar@fiverr.com>
2024-05-11 16:02:47 +0300

    .

--- mteb_test.py
+++ mteb_test.py
@@ -1,11 +1,12 @@
+import torch
 from mteb import MTEB
 
 from EasyLM.models.rpt.rpt_model_torch import RPTForCausalLM
 
+torch.set_default_device('cuda')
 
 model = RPTForCausalLM.from_pretrained('shahar603/rpt-torch-1')
 model.to('cuda')
-model.device = 'cuda'
 #model.push_to_hub(repo_id="rpt-torch-1", token='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')
 
 model.encode(['hello world', 'this is a test'])
