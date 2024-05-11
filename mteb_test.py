d80cdac253cec07ed680711a0e4d337e2a362227
Shahar Satamkar <shahar.satamkar@fiverr.com>
2024-05-11 16:32:19 +0300

    .

--- mteb_test.py
+++ mteb_test.py
@@ -5,9 +5,11 @@ from EasyLM.models.rpt.rpt_model_torch import RPTForCausalLM
 
 model = RPTForCausalLM.from_pretrained('shahar603/rpt-torch-1')
 
-device = torch.device("cuda:1,3" if torch.cuda.is_available() else "cpu") ## specify the GPU id's, GPU id's start from 0.
+import torch
+for i in range(torch.cuda.device_count()):
+   print(torch.cuda.get_device_properties(i).name)
 
-model.to(device)
+model.to('cuda')
 #model.push_to_hub(repo_id="rpt-torch-1", token='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')
 
 #model.encode(['hello world', 'this is a test'])
