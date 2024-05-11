072a70272caf0ab408e06df6d08e74e281075516
Shahar Satamkar <shahar.satamkar@fiverr.com>
2024-05-11 15:42:28 +0300

    moving to cuda

--- mteb_test.py
+++ mteb_test.py
@@ -4,6 +4,7 @@ from EasyLM.models.rpt.rpt_model_torch import RPTForCausalLM
 
 
 model = RPTForCausalLM.from_pretrained('shahar603/rpt-torch-1')
+model.to('cuda')
 #model.push_to_hub(repo_id="rpt-torch-1", token='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')
 
 model.encode(['hello world', 'this is a test'])
