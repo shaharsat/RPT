e952c77fbd7aa68e13abe11cf1ca4b01e8dcf1b8
Shahar Satamkar <shahar.satamkar@fiverr.com>
2024-05-11 16:28:07 +0300

    .

--- mteb_test.py
+++ mteb_test.py
@@ -7,8 +7,8 @@ model = RPTForCausalLM.from_pretrained('shahar603/rpt-torch-1')
 model.to('cuda')
 #model.push_to_hub(repo_id="rpt-torch-1", token='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')
 
-model.encode(['hello world', 'this is a test'])
+#model.encode(['hello world', 'this is a test'])
 
-#evaluation = MTEB(tasks=["Banking77Classification"])
-#results = evaluation.run(model, output_folder=f"results/'rpt")
+evaluation = MTEB(tasks=["Banking77Classification"])
+results = evaluation.run(model, output_folder=f"results/'rpt")
 
