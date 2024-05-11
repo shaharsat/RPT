add595ca1d380f6462a4c6f71ba1518f46cbefee
Shahar Satamkar <shahar.satamkar@fiverr.com>
2024-05-11 19:39:11 +0300

    .

--- mteb_test.py
+++ mteb_test.py
@@ -2,15 +2,15 @@ import torch
 from mteb import MTEB
 
 from EasyLM.models.rpt.rpt_model_torch import RPTForCausalLM
-#model = RPTForCausalLM.from_pretrained('shahar603/rpt-torch-1')
-#device = 'cuda' if torch.cuda.is_available() else 'cpu'
-#model.to(device)
+model = RPTForCausalLM.from_pretrained('shahar603/rpt-torch-1')
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+model.to(device)
 #model.encode(['hello world', 'this is a test'])
 
-from sentence_transformers import SentenceTransformer
+#rom sentence_transformers import SentenceTransformer
 
-model_name = "average_word_embeddings_komninos"
-model = SentenceTransformer(model_name)
+#model_name = "average_word_embeddings_komninos"
+#model = SentenceTransformer(model_name)
 
 evaluation = MTEB(tasks=["Banking77Classification"], n_experiments=1)
 results = evaluation.run(model, output_folder=f"results/rpt", batch_size=3, show_progress_bar=True)
