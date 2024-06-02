9f971807d831cdbc9936d800cdcfab82d3cdf25e
Shahar Satamkar <shahar.satamkar@fiverr.com>
2024-06-02 07:35:50 +0300

    sending the model to the GPU

--- EasyLM/models/neox/mteb_test.py
+++ EasyLM/models/neox/mteb_test.py
@@ -1,3 +1,4 @@
+import torch
 from mteb import AmazonReviewsClassification, Banking77Classification
 from EasyLM.models.neox.neox_model_torch import GPTNeoXForCausalLM
 import mteb
@@ -9,9 +10,11 @@ hf_model = GPTNeoXForCausalLM.from_pretrained(
 )
 """
 
-model_name = 'neox_rpt_model'
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+model_name = 'neox_rpt_model'
 hf_model = GPTNeoXForCausalLM.from_pretrained('Shahar603/neox-rpt-1')
+hf_model.to(device)
 
 evaluation = mteb.MTEB(tasks=[Banking77Classification(hf_subsets=["en"], batch_size=2)])
 results = evaluation.run(hf_model, output_folder=f"results/{model_name}")
